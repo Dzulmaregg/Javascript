c76307e5a69f9da7160c39c8683731eb966697b0
Dzulmar IAZ <DIAZendog@Gmail.com>
2017-10-17 00:17:55 +0700

    Add files via upload

--- jtak.js
+++ jtak.js
@@ -415,15 +415,3 @@ $('#comment_block').textWalk(function() {
         this.data = this.data.replace(new RegExp(val, "g"), array[val]);
     }
 });
-
-$('#comment_block').find('a.allow').filter(function() {
-			return /(adlmruz\.blogspot\.com|adlmruz\.blogspot\.com)?.*?\/.*?\?showComment\=[0-9]+\#c[0-9]+/.test(this.href);
-		}).attr({'title':'Klik untuk memuat jawaban/pertanyaan sejenis','oncontextmenu':'return false'}).on("click", function(a1,a2) {
-			a1 = this.href;
-			a2 = a1.split('#');
-			$(this).closest('.comment_body').html('<div class="loader" style="height:100px;background-position:50% 50%;"></div>').load(a2[0].split('?')[0]+' #'+a2[1], function() {
-				$(this).children().replaceWith($(this).html()+'<br><cite class="note">Tautan sumber: <a class="allow" href="'+a1+'">'+a1+'</a></cite><br>');
-				$(this).find('.comment_child').remove();
-			});
-			return false;
-		});
